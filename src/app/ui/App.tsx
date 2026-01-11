8ddd451adef8ecb9c418261846e4dd510f35970f
ryo-murai <murai.ry@gmail.com>
2026-01-11 12:55:13 +0900

    add default mui theme

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@mui/material";
 import "../styles/App.css";
 import { router } from "../routes/router";
 import { createAppQueryClient } from "../lib/query-client";
+import { theme } from "@/shared/ui/config/theme";
 
 function App() {
   return (
     <QueryClientProvider client={createAppQueryClient()}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/shared/ui/config/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "light", // 'light' or 'dark'
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+});
